deab1cf196d633e8c8b827550f9e09ccc5bad863
Rekinowy <107859450+Rekinowy@users.noreply.github.com>
2023-11-20 16:11:30 +0100

    fix info block height

--- app/(root)/(home)/faq/page.tsx
+++ app/(root)/(home)/faq/page.tsx
@@ -35,7 +35,7 @@ const Page = () => {
 
             <div
               className={`overflow-hidden transition-max-height duration-300 ${
-                openAnswers[index] ? "max-h-full"" : "max-h-0"
+                openAnswers[index] ? "max-h-full" : "max-h-0"
               }  text-slate-400 sm:text-lg
               `}
             >
